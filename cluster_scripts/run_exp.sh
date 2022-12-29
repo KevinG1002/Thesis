f8090b18ca63b5fb1100eb0fd101a7342db599a8
KevinG1002 <golan.kevin@gmail.com>
2022-12-29 15:40:20 +0100

    bash script to submit job on eth cluster

--- cluster_scripts/run_exp.sh
+++ cluster_scripts/run_exp.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-BATCH_SIZE=$1
-EPOCHS=$2
-LR=$3
-NAME_JOBS=$4
+# BATCH_SIZE=$1
+# EPOCHS=$2
+# LR=$3
+# NAME_JOBS=$4
 
-#SBATCH  --output=log/%j.out
-#SBATCH  --gres=gpu:1
-#SBATCH  --mem=30G
-#SBATCH --time=10:00:00
+#SBATCH --output=logs/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30G
+#!SBATCH --time=10:00:00
 
 #! What e-mail address to use for notifications?
 #! Insert your mail address here for job notifications
 #! Remove the ! to uncomment
 
 
-source ~/Documents/t_env/activate
-python -u training_good.py "$@"
+source /scratch/kgolan/t_env/bin/activate
+echo "Virtual Environment activated"
+python -u  /scratch/kgolan/Thesis/experiments/diffusion_training.py 
 echo "Deactivating Virtual Environment"
