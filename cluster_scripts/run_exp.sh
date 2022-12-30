bd771e56037047d21faa25612733fcbbaef549b2
KevinG1002 <golan.kevin@gmail.com>
2022-12-30 12:38:20 +0100

    bash script set up

--- cluster_scripts/run_exp.sh
+++ cluster_scripts/run_exp.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-
-# BATCH_SIZE=$1
-# EPOCHS=$2
-# LR=$3
-# NAME_JOBS=$4
-
-#SBATCH --output=logs/%j.out
+#SBATCH --output=/scratch_net/bmicdl03/kgolan/Thesis/cluster_scripts/logs/%j.out
 #SBATCH --gres=gpu:1
-#SBATCH --mem=30G
-#!SBATCH --time=10:00:00
+#SBATCH --mem=8G
 
-#! What e-mail address to use for notifications?
-#! Insert your mail address here for job notifications
-#! Remove the ! to uncomment
+# cd /scratch_net/bmicdl03/kgolan/Thesis
+# source /scratch_net/bmicdl03/kgolan/conda/etc/profile.d/conda.sh
+source /scratch_net/bmicdl03/kgolan/venv/bin/activate
+# conda activate /scratch_net/bmicdl03/kgolan/conda_envs/t_env
 
-
-source /scratch/kgolan/t_env/bin/activate
 echo "Virtual Environment activated"
-python -u  /scratch/kgolan/Thesis/experiments/diffusion_training.py 
+python -u  /scratch_net/bmicdl03/kgolan/Thesis/experiments/diffusion_training.py 
 echo "Deactivating Virtual Environment"
+# conda deactivate
+deactivate
