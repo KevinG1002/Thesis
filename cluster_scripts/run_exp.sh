f88d35bc8201e77b7b711168350fdcc644b8e7c3
KevinG1002 <golan.kevin@gmail.com>
2023-01-11 17:36:01 +0100

    cd into experiments folder so that experimental_results dir can be created

--- cluster_scripts/run_exp.sh
+++ cluster_scripts/run_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --output=/scratch_net/bmicdl03/kgolan/Thesis/cluster_scripts/logs/%j.out
+#SBATCH --output=/scratch_net/bmicdl03/kgolan/Thesis/experiments/experimental_results/%j.out
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8G
 #! What e-mail address to use for notifications?
@@ -12,10 +12,12 @@
 #! Remove the ! to uncomment
 #SBATCH --mail-type=ALL
 
-source /scratch_net/bmicdl03/kgolan/venv/bin/activate
-cd /scratch_net/bmicdl03/kgolan/Thesis
+source /scratch_net/bmicdl03/kgolan/conda/etc/profile.d/conda.sh
+conda activate m_env
+cd /scratch_net/bmicdl03/kgolan/Thesis/
 pip install -e .
+cd /scratch_net/bmicdl03/kgolan/Thesis/experiments/
 echo "Virtual Environment activated"
-python -u  /scratch_net/bmicdl03/kgolan/Thesis/experiments/diffusion_training.py 
+python -u  /scratch_net/bmicdl03/kgolan/Thesis/experiments/snapshot_training.py -e 10 --M_snapshots 5 --save_every 2 -lr 0.01
 echo "Deactivating Virtual Environment"
 deactivate
