a21d309d58fd5ba5e34623744dd3f57a03a8b83b
KevinG1002 <golan.kevin@gmail.com>
2023-01-17 17:58:19 +0100

    profiling script showing memory consumption before and after method/function execution

--- utils/profile.py
+++ utils/profile.py
@@ -6,55 +6,25 @@ import inspect
 
 
 def elapsed_since(start):
-    # return time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
-    elapsed = time.time() - start
-    if elapsed < 1:
-        return str(round(elapsed * 1000, 2)) + "ms"
-    if elapsed < 60:
-        return str(round(elapsed, 2)) + "s"
-    if elapsed < 3600:
-        return str(round(elapsed / 60, 2)) + "min"
-    else:
-        return str(round(elapsed / 3600, 2)) + "hrs"
-
-
+    return time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+ 
+ 
 def get_process_memory():
     process = psutil.Process(os.getpid())
-    mi = process.memory_info()
-    return mi.rss, mi.vms, mi.shared
-
-
-def format_bytes(bytes):
-    if abs(bytes) < 1000:
-        return str(bytes) + "B"
-    elif abs(bytes) < 1e6:
-        return str(round(bytes / 1e3, 2)) + "kB"
-    elif abs(bytes) < 1e9:
-        return str(round(bytes / 1e6, 2)) + "MB"
-    else:
-        return str(round(bytes / 1e9, 2)) + "GB"
-
-
-def profile(func, *args, **kwargs):
+    mem_info = process.memory_info()
+    return mem_info.rss
+ 
+ 
+def profile(func):
     def wrapper(*args, **kwargs):
-        rss_before, vms_before, shared_before = get_process_memory()
+        mem_before = get_process_memory()
         start = time.time()
         result = func(*args, **kwargs)
         elapsed_time = elapsed_since(start)
-        rss_after, vms_after, shared_after = get_process_memory()
-        print(
-            "Profiling: {:>20}  RSS: {:>8} | VMS: {:>8} | SHR {"
-            ":>8} | time: {:>8}".format(
-                "<" + func.__name__ + ">",
-                format_bytes(rss_after - rss_before),
-                format_bytes(vms_after - vms_before),
-                format_bytes(shared_after - shared_before),
-                elapsed_time,
-            )
-        )
+        mem_after = get_process_memory()
+        print("{}: memory before: {:,}, after: {:,}, consumed: {:,}; exec time: {}".format(
+            func.__name__,
+            mem_before, mem_after, mem_after - mem_before,
+            elapsed_time))
         return result
-
-    if inspect.isfunction(func):
-        return wrapper
-    elif inspect.ismethod(func):
-        return wrapper(*args, **kwargs)
+    return wrapper
