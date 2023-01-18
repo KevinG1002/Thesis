0d33b5c0d2723806417800387b43b3ed502b0339
KevinG1002 <golan.kevin@gmail.com>
2023-01-18 12:56:27 +0100

    new script for graph manipulation and conversion from weight matrices to graphs

---
+++ utils/graph_manipulations.py
@@ -0,0 +1,5 @@
+# Script of helper functions that will be used to convert weight tensors to a graph representation for graph networks;
+import torch
+
+def weight_tensor_to_graph(weights: torch.Tensor):
+    pass
