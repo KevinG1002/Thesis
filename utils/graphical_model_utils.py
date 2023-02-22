73a3ffad60a66ae90512683ef0b24698670630d6
Kevin Golan <golan.kevin@gmail.com>
2023-02-22 15:17:57 -0500

    util to convert samples to NN Module

--- utils/graphical_model_utils.py
+++ utils/graphical_model_utils.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+import copy
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def split_weights_biases(nn: nn.Module):
@@ -41,3 +44,23 @@ def layer_wise_dataset(nn_dataset: list[list]):
     layerwise_weight_datasets = tensor_dataset_layer_wise(len(weights), weight_dataset)
     layerwise_bias_datasets = tensor_dataset_layer_wise(len(weights), bias_dataset)
     return layerwise_weight_datasets, layerwise_bias_datasets
+
+
+def sample_dict_to_module(base_model: nn.Module, sample_dict: dict):
+    base_model = base_model.to(DEVICE)
+    new_nn = copy.deepcopy(base_model)
+    new_state_dict = dict.fromkeys(new_nn.state_dict())
+    weight_keys = [k for k in sample_dict.keys() if k.startswith("w_")]
+    bias_keys = [k for k in sample_dict.keys() if k.startswith("b_")]
+    b_idx = 0
+    w_idx = 0
+    for idx, layer in enumerate(base_model.state_dict().keys()):
+        if idx % 2 == 0:
+            new_state_dict[layer] = sample_dict[weight_keys[w_idx]]
+            w_idx += 1
+        else:
+            new_state_dict[layer] = sample_dict[bias_keys[b_idx]]
+            b_idx += 1
+
+    new_nn.load_state_dict(new_state_dict)
+    return new_nn
